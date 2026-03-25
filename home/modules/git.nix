be6f67a7252b629cc028d1ecde2c175b75123a58
Asytuyf <nabaoui21@gmail.com>
2026-03-25 17:54:47 +0100

    fix: git credential helper path

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -3,20 +3,12 @@
 {
   programs.git = {
     enable = true;
-    settings = {
-      user = {
-        name = "Asytuyf";
-        email = "nabaoui21@gmail.com";
-      };
-      init = {
-        defaultBranch = "main";
-      };
-      credential = {
-        helper = "!gh auth git-credential";
-      };
-      safe = {
-        directory = "/etc/nixos";
-      };
+    userName = "Asytuyf";
+    userEmail = "nabaoui21@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+      credential.helper = "/run/current-system/sw/bin/gh auth git-credential";
+      safe.directory = "/etc/nixos";
     };
   };
 }
